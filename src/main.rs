9b5ed793ace0edbf144a1df8893d57019c304bac
gescalona1 <gescalo2@u.rochester.edu>
2022-05-18 13:12:43 -0400

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,15 @@
+use std::io;
 
-fn average(arr: &[i32]) -> i32 {
-    let mut sum: i32 = 0;
-    for number in arr {
-        sum = sum + number;
-    }
-    let size: i32 = arr.len() as i32;
-    sum / size
+fn average(arr: &[i32]) -> f32 {
+    let sum: i32 = arr.iter().sum();
+    let size = arr.len();
+    sum as f32 / size as f32
 }
 
 fn main() {
     println!("Hello, world!");
     //0 1 2 3 4 5
-    let arr: [i32; 5] =  [4, 3, 2, 1, 100];
+    let arr =  [4, 3, 2, 1, 100, 50, 200];
     println!("My average is {}", average(&arr));
     
 }
